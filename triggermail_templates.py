0f1239fafd2e9d8890c0c9a63613908d5da458eb
Mahmoud Arram <mahmoud.arram@gmail.com>
2013-07-25 11:44:10 -0400

    plugin updates

--- triggermail_templates.py
+++ triggermail_templates.py
@@ -8,7 +8,7 @@ import webbrowser
 import requests
 
 try:
-    settings = sublime.load_settings('TriggerMail_Templates.sublime-settings')
+    settings = sublime.load_settings('TriggerMail.sublime-settings')
 except:
     settings = {}
 
@@ -26,12 +26,15 @@ class PreviewTemplateCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         filename = self.view.file_name()
         if not filename:
-            sublime.error_message("You have to provide a template path.")
-
+            return sublime.error_message("You have to provide a template path.")
+        if not filename.endswith(".html"):
+            return sublime.error_message("Invalid html template %s" % filename)
         if not os.path.exists(filename):
-            sublime.error_message("File does not exist")
+            return sublime.error_message("File does not exist")
+
         path = os.path.dirname(filename)
         action = filename.replace(path, "").replace(".html", "").strip(os.sep)
+
         # Read all the files in the given folder.
         # We gather them all and then send them up to GAE.
         # We do this rather than processing template locally. Because local processing
@@ -43,10 +46,15 @@ class PreviewTemplateCommand(sublime_plugin.TextCommand):
                     file_map[filename] = contents
 
         partner = path.split(os.sep)[-1]
+        # You can override the partner in the settings file
+        partner = settings.get("partner", partner)
         print "Attempting to render %s for %s" % (action, partner)
 
-        request = requests.post(settings.get("engine", "http://www.triggermail.io/api/templates/render_raw_template"), data=dict(templates=json.dumps(file_map), partner=partner, action=action))
+        request = requests.post(settings.get("engine", "http://www.triggermail.io/api/templates/render_raw_template"), data=dict(templates=json.dumps(file_map), partner=partner, action=action, format="json"))
         response = request.text
+        if request.status_code == 500:
+            return sublime.error_message(request.json().get("message"))
+
         temp = tempfile.NamedTemporaryFile(delete=False, suffix=".html")
         temp.write(response.encode("utf-8"))
         temp.close()
